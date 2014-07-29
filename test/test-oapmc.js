f490d133b1095637517fe16a624ba9212f2d961a
sballesteros <sebastien.ballesteros@gmail.com>
2014-07-29 01:15:37 -0400

    authors, editors and accountable persons

--- test/test-oapmc.js
+++ test/test-oapmc.js
@@ -48,7 +48,7 @@ describe('pubmed central', function(){
 
   this.timeout(4000);
 
-  describe('metadata', function(){
+  describe.skip('metadata', function(){
 
     //http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:2924383&metadataPrefix=pmc
     it('should create a package.jsonld for a ms with a movie zipped and not treat it as a code bundle', function(done){
@@ -100,8 +100,8 @@ describe('pubmed central', function(){
       getPkg('PMC3532326', function(err, pkg, files, inlines, $doc){
         oapmc._html(pkg, files, inlines, $doc, function(err, html){
           if(err) throw err;          
-          //          var $HOME = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-          //          fs.writeFileSync(path.join($HOME, 'Desktop/pm.html'), html, {encoding: 'utf8'});
+          var $HOME = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+          fs.writeFileSync(path.join($HOME, 'Desktop/pm.html'), html, {encoding: 'utf8'});
           //          console.log(html);
           done();
         });
